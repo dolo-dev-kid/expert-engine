b4443352f1bc4e5e238f39ed7abd9c315fdd93b0
dolo-dev-kid <robinsonwesley206@gmail.com>
2026-09-13 21:09:02 -0700

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-joined", socket.id);
+  });
+
+  socket.on("offer", (data) => {
+    socket.to(data.room).emit("offer", {
+      sdp: data.sdp,
+      from: socket.id
+    });
+  });
+
+  socket.on("answer", (data) => {
+    socket.to(data.room).emit("answer", {
+      sdp: data.sdp,
+      from: socket.id
+    });
+  });
+
+  socket.on("ice-candidate", (data) => {
+    socket.to(data.room).emit("ice-candidate", {
+      candidate: data.candidate,
+      from: socket.id
+    });
+  });
+
+  socket.on("chat-message", (data) => {
+    io.to(data.room).emit("chat-message", {
+      message: data.message,
+      from: socket.id
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Expert Engine backend running");
+});
+
+server.listen(3001, () => {
+  console.log("Backend running on http://localhost:3001");
+});
