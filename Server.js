02b9c76261c5ef073599612976f26c5fe278b782
dolo-dev-kid <robinsonwesley206@gmail.com>
2026-09-14 19:33:29 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,4 +1,3 @@
-// backend/Server.js
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
@@ -16,41 +15,52 @@ const io = new Server(server, {
   }
 });
 
-// --- WebRTC + Chat Signaling ---
+// ROOM STATE
+const rooms = {}; 
+// rooms[roomId] = [socketId, socketId, ...]
+
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  console.log("user connected:", socket.id);
 
-  // Join a room
+  // JOIN ROOM
   socket.on("join-room", (roomId) => {
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push(socket.id);
+
     socket.join(roomId);
+
+    // Tell existing users about the new user
     socket.to(roomId).emit("user-joined", socket.id);
+
+    // Tell new user who is already in the room
+    io.to(socket.id).emit("existing-users", rooms[roomId].filter(id => id !== socket.id));
   });
 
-  // WebRTC offer
+  // OFFER (mesh)
   socket.on("offer", (data) => {
-    socket.to(data.room).emit("offer", {
+    io.to(data.to).emit("offer", {
       sdp: data.sdp,
       from: socket.id
     });
   });
 
-  // WebRTC answer
+  // ANSWER (mesh)
   socket.on("answer", (data) => {
-    socket.to(data.room).emit("answer", {
+    io.to(data.to).emit("answer", {
       sdp: data.sdp,
       from: socket.id
     });
   });
 
-  // ICE candidates
+  // ICE CANDIDATE (mesh)
   socket.on("ice-candidate", (data) => {
-    socket.to(data.room).emit("ice-candidate", {
+    io.to(data.to).emit("ice-candidate", {
       candidate: data.candidate,
       from: socket.id
     });
   });
 
-  // Group chat
+  // GROUP CHAT
   socket.on("chat-message", (data) => {
     io.to(data.room).emit("chat-message", {
       message: data.message,
@@ -58,15 +68,26 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  // GROUP STREAM BROADCAST
+  socket.on("stream-data", (data) => {
+    socket.to(data.room).emit("stream-data", {
+      chunk: data.chunk,
+      from: socket.id
+    });
   });
-});
 
-app.get("/", (req, res) => {
-  res.send("Expert Engine backend running");
+  // DISCONNECT
+  socket.on("disconnect", () => {
+    console.log("user disconnected:", socket.id);
+
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+      socket.to(roomId).emit("user-left", socket.id);
+    }
+  });
 });
 
 server.listen(3001, () => {
   console.log("Backend running on http://localhost:3001");
 });
+
