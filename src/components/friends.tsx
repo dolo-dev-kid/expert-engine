62aa795f75f9f426dc34cb3bb038ac10e65f0a88
dolo-dev-kid <robinsonwesley206@gmail.com>
2026-09-14 21:55:13 -0700

    Create friends.tsx

---
+++ src/components/friends.tsx
@@ -0,0 +1,43 @@
+export default function Profile() {
+  return (
+    <div style={{ padding: 40, color: "white" }}>
+      <h1>Your Profile</h1>
+
+      <div
+        style={{
+          marginTop: 20,
+          background: "#1e1f22",
+          padding: 20,
+          borderRadius: 12,
+          width: 400
+        }}
+      >
+        <img
+          src="https://i.imgur.com/4ZQZ4ZQ.png"
+          style={{
+            width: 120,
+            height: 120,
+            borderRadius: "50%",
+            marginBottom: 20
+          }}
+        />
+
+        <h2>Wesley</h2>
+        <p style={{ marginTop: 10 }}>Streamer • Creator • Dreamer</p>
+
+        <button
+          style={{
+            marginTop: 20,
+            padding: "12px 20px",
+            borderRadius: 8,
+            background: "#00eaff",
+            border: "none",
+            cursor: "pointer"
+          }}
+        >
+          Edit Profile
+        </button>
+      </div>
+    </div>
+  );
+}
